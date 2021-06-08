0324b376c91aeb1158363c0c45221c7c7412e864
Brian Davis <bscottdavis@gmail.com>
2021-06-07 17:14:28 -0700

    Added View, Session and Person Columns

--- CMS/Message Notes/AppPageViewsByDate.sql
+++ CMS/Message Notes/AppPageViewsByDate.sql
@@ -1,5 +1,6 @@
-SELECT CAST(i.CreatedDateTime AS date) [Date], COUNT(DISTINCT i.InteractionSessionId) PageViews
+SELECT CAST(i.CreatedDateTime AS date) [Date], COUNT(DISTINCT i.Id) [Views], COUNT(DISTINCT i.InteractionSessionId) [Sessions], COUNT(DISTINCT pa.PersonId) LoggedInPeople
 FROM Interaction i 
+LEFT JOIN PersonAlias pa ON i.PersonAliasId = pa.Id
 WHERE i.InteractionComponentId = 114866
 GROUP BY CAST(i.CreatedDateTime AS date)
 ORDER BY CAST(i.CreatedDateTime AS date) DESC
