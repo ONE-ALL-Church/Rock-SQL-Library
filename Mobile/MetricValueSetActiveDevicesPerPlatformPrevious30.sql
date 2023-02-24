43c083ecfc137e8c197076ec6d28478c077136c3
Brian Davis <bscottdavis@gmail.com>
2023-02-24 12:28:40 -0800

    Improved Effeciency

--- Mobile/MetricValueSetActiveDevicesPerPlatformPrevious30.sql
+++ Mobile/MetricValueSetActiveDevicesPerPlatformPrevious30.sql
@@ -2,20 +2,19 @@
 -- Tracked At: Mobile/MetricValueSetActiveDevicesPerPlatformPrevious30.sql
 
 DECLARE @OADateTime DATE
-SET @OADateTime = CAST(GETUTCDATE() AT TIME ZONE 'Pacific Standard Time' AS date);
+SET @OADateTime = CONVERT(DATE, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time');
 DECLARE @Cutoff DATE
-SET @Cutoff = CAST(DATEADD(d, -8, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time') AS date);
+SET @Cutoff = CONVERT(DATE, DATEADD(d, -15, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time'));
 
 SELECT COUNT(DISTINCT i.PersonalDeviceId) Devices,
 asd.[Date] [MetricValueDateTime],
 pdplatdv.Id Platform
 FROM AnalyticsSourceDate asd
-INNER JOIN InteractionChannel ichan ON ichan.Id = 15
-INNER JOIN InteractionComponent icom ON ichan.Id = icom.InteractionChannelId
-INNER JOIN AnalyticsSourceDate asdSeven ON asdSeven.[Date] <= asd.[Date] AND asdSeven.[Date] > DATEADD(d,-30, asd.[Date])
-INNER JOIN Interaction i ON i.InteractionDateKey  =  asdSeven.DateKey
-INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id 
+INNER JOIN Interaction i ON i.InteractionDateTime BETWEEN DATEADD(d, -29, asd.[Date]) AND DATEADD(d, 1, asd.[Date])
+INNER JOIN InteractionComponent icom ON i.InteractionComponentId = icom.Id
+INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id
 INNER JOIN DefinedValue pdplatdv ON pd.PlatformValueId = pdplatdv.Id
-WHERE  asd.[Date] >= @Cutoff AND asd.[Date] < @OADateTime
-GROUP BY asd.Date, pdplatdv.Id
+WHERE asd.[Date] >= @Cutoff AND asd.[Date] < @OADateTime
+AND icom.InteractionChannelId = 15
+GROUP BY asd.[Date], pdplatdv.Id
 ORDER BY asd.[Date]
