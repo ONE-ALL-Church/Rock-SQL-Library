cad940358cc1d882164fa0d9ae21affe63353fb7
Brian Davis <bscottdavis@gmail.com>
2023-03-01 12:05:14 -0800

    Improve Effeciency Remove Unnecesary Joins

--- Mobile/MetricValueSetNewDevicesByDay.sql
+++ Mobile/MetricValueSetNewDevicesByDay.sql
@@ -8,9 +8,6 @@ FROM (
     INNER JOIN InteractionComponent icom ON ichan.Id = icom.InteractionChannelId
     INNER JOIN Interaction i ON icom.Id = i.InteractionComponentId
     INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id
-    INNER JOIN DefinedValue pddv ON pd.PersonalDeviceTypeValueId = pddv.Id
-    INNER JOIN DefinedValue pdplatdv ON pd.PlatformValueId = pdplatdv.Id
-    INNER JOIN InteractionSession ises ON i.InteractionSessionId = ises.Id
     WHERE ichan.Id = 15
         AND i.[InteractionDateTime] >= '2020-10-03'
     GROUP BY pd.Id
