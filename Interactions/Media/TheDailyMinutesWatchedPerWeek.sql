5ba8abc123d321a516ff0fca64cfd2de140becf2
Brian Davis <bscottdavis@gmail.com>
2023-05-01 15:38:57 -0700

    Add Weekly Unique Viewers

--- Interactions/Media/TheDailyMinutesWatchedPerWeek.sql
+++ Interactions/Media/TheDailyMinutesWatchedPerWeek.sql
@@ -1,37 +1,31 @@
-SELECT asd.SundayDate
-    , ROUND(SUM(i.MinutesWatched),0) MinutesWatched
-    , SUM(i.UniqueWatchers) Views
-    , SUM(i.TotalInteractions) TotalInteractions
-FROM (
-    SELECT cci.StartDateTime,
-        cci.Title,
-        dvSpeaker.[Value],
-        (SUM(i.InteractionLength) / 100 * me.DurationSeconds) / 60 MinutesWatched,
-        COUNT(DISTINCT pa.PersonId) UniqueWatchers,
-        COUNT(i.Id) TotalInteractions,
-        i.InteractionDateKey
+WITH InteractionSummary
+AS (
+    SELECT asd.DateKey AS InteractionDateKey,
+        me.Id AS MediaElementId,
+        pa.PersonId,
+        (SUM(i.InteractionLength) * 0.01 * me.DurationSeconds) / 60 AS MinutesWatched,
+        COUNT(i.Id) AS TotalInteractions
     FROM Interaction i
-    INNER JOIN AnalyticsSourceDate asd ON i.InteractionDateKey = asd.DateKey
-    INNER JOIN InteractionComponent ic ON i.InteractionComponentId = ic.Id
+    JOIN AnalyticsSourceDate asd ON i.InteractionDateKey = asd.DateKey
+    JOIN InteractionComponent ic ON i.InteractionComponentId = ic.Id
         AND ic.InteractionChannelId = 28
-    INNER JOIN MediaElement me ON ic.EntityId = me.Id
+    JOIN MediaElement me ON ic.EntityId = me.Id
     LEFT JOIN PersonAlias pa ON i.PersonAliasId = pa.Id
-    INNER JOIN AttributeValue av ON TRY_CAST(av.[Value] AS UNIQUEIDENTIFIER) = me.Guid
+    JOIN AttributeValue av ON TRY_CAST(av.[Value] AS UNIQUEIDENTIFIER) = me.Guid
         AND av.AttributeId = 29293
-    INNER JOIN ContentChannelItem cci ON av.EntityId = cci.Id
-    LEFT JOIN AttributeValue avSpeaker ON avSpeaker.[EntityId] = cci.Id
-        AND avSpeaker.AttributeId = 29299
-    LEFT JOIN DefinedValue dvSpeaker ON TRY_CAST(avSpeaker.[Value] AS UNIQUEIDENTIFIER) = dvSpeaker.Guid
+    JOIN ContentChannelItem cci ON av.EntityId = cci.Id
     WHERE i.InteractionLength IS NOT NULL
-    GROUP BY i.InteractionDateKey,
+    GROUP BY asd.DateKey,
         me.Id,
-        me.Name,
         me.DurationSeconds,
-        me.CreatedDateTime,
-        cci.StartDateTime,
-        cci.Title,
-        dvSpeaker.[Value]
-    ) i
-INNER JOIN AnalyticsSourceDate asd ON i.InteractionDateKey = asd.DateKey
+        pa.PersonId
+    )
+SELECT asd.SundayDate,
+    ROUND(SUM(isum.MinutesWatched), 0) AS MinutesWatched,
+    COUNT(*) AS SumDailyViewers,
+    COUNT(DISTINCT isum.PersonId) AS WeeklyUniqueViewers,
+    SUM(isum.TotalInteractions) AS TotalInteractions
+FROM InteractionSummary isum
+JOIN AnalyticsSourceDate asd ON isum.InteractionDateKey = asd.DateKey
 GROUP BY asd.SundayDate
 ORDER BY asd.SundayDate DESC
