5e33a7eaf3ba8a56f1b7d4a0c30c97dc0c6bad11
Brian Davis <bscottdavis@gmail.com>
2023-05-22 13:37:29 -0700

    Add Percent Complete

--- Interactions/TheDaily/TheDailyPageViews.sql
+++ Interactions/TheDaily/TheDailyPageViews.sql
@@ -6,41 +6,13 @@ AS (
     FROM Interaction i
     INNER JOIN PersonAlias pa ON i.PersonAliasId = pa.Id
     INNER JOIN Person p ON pa.PersonId = p.Id
-    WHERE i.InteractionComponentId IN (326528, 326808, 326548, 326566,114861) -- 109365 Home Page For Testing, 326808
+    WHERE i.InteractionComponentId IN (326528, 326808, 326548, 326566, 114861) -- 109365 Home Page For Testing, 326808
         --   AND i.InteractionDateTime < CAST(GETDATE() AS DATE)
     GROUP BY CAST(i.InteractionDateTime AS DATE),
         i.InteractionComponentId,
         p.Id
-    ),
-Streaks
-AS (
-    SELECT InteractionDate,
-        CASE 
-            WHEN LAG(InteractionDate) OVER (
-                    ORDER BY InteractionDate
-                    ) = DATEADD(day, 0, InteractionDate)
-                OR (
-                    DATEPART(weekday, InteractionDate) = 2
-                    AND DATEADD(day, - 3, InteractionDate) = LAG(InteractionDate) OVER (
-                        ORDER BY InteractionDate
-                        )
-                    )
-                THEN 1
-            ELSE 0
-            END AS IsConsecutive
-    FROM InteractionDates
-    ),
-CurrentStreak
-AS (
-    SELECT SUM(IsConsecutive) AS Streak
-    FROM Streaks
-    WHERE InteractionDate >= (
-            SELECT MAX(InteractionDate)
-            FROM Streaks
-            WHERE IsConsecutive = 0
-            )
     )
-SELECT id.InteractionDate,
+SELECT idStart.InteractionDate,
     COUNT(DISTINCT id.Id) [Start],
     COUNT(DISTINCT CASE 
             WHEN id.InteractionComponentId = 326548
@@ -61,11 +33,21 @@ SELECT id.InteractionDate,
             WHEN id.InteractionComponentId = 326808
                 THEN id.Id
             ELSE NULL
-            END) [Success]
+            END) [Success],
+    CASE 
+        WHEN COUNT(DISTINCT id.Id) != 0
+            THEN FORMAT(CAST(COUNT(DISTINCT CASE 
+                                WHEN id.InteractionComponentId = 326808
+                                    THEN id.Id
+                                ELSE NULL
+                                END) AS FLOAT) / CAST(COUNT(DISTINCT id.Id) AS FLOAT), 'P')
+        ELSE NULL
+        END [PercentCompleted]
 FROM InteractionDates idStart
 INNER JOIN InteractionDates id ON id.InteractionDate = idStart.InteractionDate
     AND id.Id = idStart.Id
 WHERE idStart.InteractionComponentId = 326528
-GROUP BY id.InteractionDate
-ORDER BY id.InteractionDate DESC
-
+    AND DATEPART(dw, idStart.InteractionDate) NOT IN (1, 7)
+    AND idStart.InteractionDate >= '2023-05-08'
+GROUP BY idStart.InteractionDate
+ORDER BY idStart.InteractionDate DESC
