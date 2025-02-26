2b788bd3e49459bcb44a47de5fb635103f2a17f6
Brian Davis <bscottdavis@gmail.com>
2025-02-26 13:26:07 -0800

    Optimizations

--- Mobile/MetricValueSetActiveDevicesPerPlatformPrevious1.sql
+++ Mobile/MetricValueSetActiveDevicesPerPlatformPrevious1.sql
@@ -1,20 +1,48 @@
 --Implemented At: https://admin.oneandall.church/reporting/metrics?MetricCategoryId=67&ExpandedIds=C758%2CC596
 -- Tracked At: Mobile/MetricValueSetActiveDevicesPerPlatformPrevious1.sql
 
-DECLARE @OADateTime DATE
+DECLARE @OADateTime DATE;
 SET @OADateTime = CONVERT(DATE, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time');
-DECLARE @Cutoff DATE
-SET @Cutoff = CONVERT(DATE, DATEADD(d, -15, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time'));
+DECLARE @Cutoff DATE;
+SET @Cutoff = CONVERT(DATE, DATEADD(day, -15, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time'));
 
-SELECT COUNT(DISTINCT i.PersonalDeviceId) Devices,
-asd.[Date] [MetricValueDateTime],
-pdplatdv.Id Platform
-FROM AnalyticsSourceDate asd
-INNER JOIN Interaction i ON i.InteractionDateTime BETWEEN DATEADD(d, -1, asd.[Date]) AND DATEADD(d, 1, asd.[Date])
-INNER JOIN InteractionComponent icom ON i.InteractionComponentId = icom.Id
-INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id
-INNER JOIN DefinedValue pdplatdv ON pd.PlatformValueId = pdplatdv.Id
-WHERE asd.[Date] >= @Cutoff AND asd.[Date] < @OADateTime
-AND icom.InteractionChannelId = 15
-GROUP BY asd.[Date], pdplatdv.Id
-ORDER BY asd.[Date]
+-- Define all date-platform combinations
+WITH DatePlatform AS (
+    SELECT 
+        asd.[Date],
+        pdplatdv.Id AS Platform
+    FROM AnalyticsSourceDate asd
+    CROSS JOIN DefinedValue pdplatdv
+    WHERE asd.[Date] >= @Cutoff 
+    AND asd.[Date] < @OADateTime
+    AND pdplatdv.Id IN (1682, 1683)
+    -- Optional: Add WHERE pdplatdv.Id IN (1682, 1683, 1684, 1817, 1818) if specific platforms are needed
+),
+
+-- Aggregate interactions by date and platform
+InteractionAggregate AS (
+    SELECT 
+        CAST(i.InteractionDateTime AS DATE) AS MetricValueDateTime,
+        pd.PlatformValueId AS Platform,
+        COUNT(DISTINCT i.PersonalDeviceId) AS Devices
+    FROM Interaction i
+    INNER JOIN InteractionComponent icom 
+        ON i.InteractionComponentId = icom.Id 
+        AND icom.InteractionChannelId = 15
+    INNER JOIN PersonalDevice pd 
+        ON i.PersonalDeviceId = pd.Id
+    WHERE i.InteractionDateTime >= @Cutoff 
+    AND i.InteractionDateTime < @OADateTime
+    GROUP BY CAST(i.InteractionDateTime AS DATE), pd.PlatformValueId
+)
+
+-- Combine date-platform pairs with interaction counts
+SELECT 
+    COALESCE(ia.Devices, 0) AS Devices,
+    dp.[Date] AS MetricValueDateTime,
+    dp.Platform
+FROM DatePlatform dp
+LEFT JOIN InteractionAggregate ia 
+    ON dp.[Date] = ia.MetricValueDateTime 
+    AND dp.Platform = ia.Platform
+ORDER BY dp.[Date], dp.Platform;
--- Mobile/MetricValueSetActiveDevicesPerPlatformPrevious7.sql
+++ Mobile/MetricValueSetActiveDevicesPerPlatformPrevious7.sql
@@ -1,20 +1,62 @@
---Implemented At: https://admin.oneandall.church/reporting/metrics?MetricCategoryId=89&ExpandedIds=C758%2CC596
--- Tracked At: Mobile/MetricValueSetActiveDevicesPerPlatformPrevious7.sql
+-- Declare variables for date range
+DECLARE @CurrentDate DATE;
 
-DECLARE @OADateTime DATE
-SET @OADateTime = CONVERT(DATE, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time');
-DECLARE @Cutoff DATE
-SET @Cutoff = CONVERT(DATE, DATEADD(d, -15, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time'));
+SET @CurrentDate = CONVERT(DATE, GETUTCDATE() AT TIME ZONE 'Pacific Standard Time');
 
-SELECT COUNT(DISTINCT i.PersonalDeviceId) Devices,
-asd.[Date] [MetricValueDateTime],
-pdplatdv.Id Platform
-FROM AnalyticsSourceDate asd
-INNER JOIN Interaction i ON i.InteractionDateTime BETWEEN DATEADD(d, -6, asd.[Date]) AND DATEADD(d, 1, asd.[Date])
-INNER JOIN InteractionComponent icom ON i.InteractionComponentId = icom.Id
-INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id
-INNER JOIN DefinedValue pdplatdv ON pd.PlatformValueId = pdplatdv.Id
-WHERE asd.[Date] >= @Cutoff AND asd.[Date] < @OADateTime
-AND icom.InteractionChannelId = 15
-GROUP BY asd.[Date], pdplatdv.Id
-ORDER BY asd.[Date]
+DECLARE @CutoffDate DATE;
+
+SET @CutoffDate = DATEADD(DAY, - 15, @CurrentDate);-- Starting 15 days back
+
+-- Generate a list of the last 15 days
+WITH DateList
+AS (
+    SELECT @CutoffDate AS MetricDate,
+        1 AS DayNum
+    
+    UNION ALL
+    
+    SELECT DATEADD(DAY, 1, MetricDate),
+        DayNum + 1
+    FROM DateList
+    WHERE DayNum < 15
+    ),
+    -- Retrieve all platforms from DefinedValue
+PlatformList
+AS (
+    SELECT Id AS Platform
+    FROM DefinedValue
+    WHERE Id IN (1682, 1683)
+    ),
+    -- Create all possible date-platform combinations
+DatePlatform
+AS (
+    SELECT dl.MetricDate,
+        pl.Platform
+    FROM DateList dl
+    CROSS JOIN PlatformList pl
+    ),
+    -- Calculate distinct device counts per date and platform
+DeviceCounts
+AS (
+    SELECT dp.MetricDate,
+        dp.Platform,
+        COUNT(DISTINCT i.PersonalDeviceId) AS Devices
+    FROM DatePlatform dp
+    LEFT JOIN (
+        Interaction i INNER JOIN InteractionComponent icom ON i.InteractionComponentId = icom.Id
+            AND icom.InteractionChannelId = 15 -- Filter for specific interaction channel
+        INNER JOIN PersonalDevice pd ON i.PersonalDeviceId = pd.Id
+        ) ON i.InteractionDateTime >= DATEADD(DAY, -6, dp.MetricDate) -- Start of 7-day window
+        AND i.InteractionDateTime < DATEADD(DAY, 1, dp.MetricDate) -- End of 7-day window
+        AND pd.PlatformValueId = dp.Platform -- Match device platform to DefinedValue Id
+    GROUP BY dp.MetricDate,
+        dp.Platform
+    )
+-- Final output with zero counts for no interactions
+SELECT 
+    COALESCE(Devices, 0) AS DevicesWithInteractions,
+    dp.MetricDate AS MetricValueDateTime,
+    dp.Platform AS Platform
+FROM DeviceCounts dp
+ORDER BY MetricValueDateTime,
+    Platform;
